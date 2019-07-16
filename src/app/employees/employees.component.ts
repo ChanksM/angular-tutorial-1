a70c811b59502262b9ccb470e1e48fd5ec264f81
ChanksM <mushni.chankseliani@geolab.edu.ge>
2019-07-16 17:32:16 +0400

    feat(animations): Finish task-1
    Finish both of tasks

--- src/app/employees/employees.component.ts
+++ src/app/employees/employees.component.ts
@@ -1,16 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { EmployeesService } from '../employees.service';
+import { trigger, state, style } from '@angular/animations';
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
-  styleUrls: ['./employees.component.scss']
+  styleUrls: ['./employees.component.scss'],
+  animations: [
+    trigger('loadUnload', [
+      state('load', style({
+        display: 'none'
+      })),
+      state('unload', style({
+        display: 'block'
+      }))
+    ])
+  ]
 })
 export class EmployeesComponent implements OnInit {
   employees$;
+  loader = true;
   constructor(private employeesService: EmployeesService) {}
 
   ngOnInit() {
     this.employees$ = this.employeesService.getEmployees();
+    this.employees$.subscribe( data => {
+      this.loader = false;
+    }
+    );
   }
 }
